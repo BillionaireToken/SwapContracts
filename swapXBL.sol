29454dd227a7e4cd0dbb36ba4be961448d15a451
gluedog <32752940+gluedog@users.noreply.github.com>
2018-12-30 15:00:51 +0200

    fixes fixes fixes,
    do I really have to update everything?

--- swapXBL.sol
+++ swapXBL.sol
@@ -8,6 +8,10 @@
 // 5. The contrak will create a database entry of the user's eosio username and the amount of XBL it has registered succesfully.
 // 6. These funds are now frozen. The database will then be used by the eosio airdrop/XBL swap contrak.
 
+// TODO: 1. Add address_to_eosio_username mapping to the registerSwap function.
+//       2. if  balance > 5000, add 5% directly in registerSwap function so it's not payable when we call getBalanceByEOSIO_USERNAME
+//       3. See if there's a simpler way to store all of this data by using only one array.
+
 
 pragma solidity ^0.5.2;
 
@@ -22,27 +26,28 @@ contract XBL_ERC20Wrapper
 
 contract SwapContrak
 {
-	string eosio_username;
-	mapping(string => uint256) registered_for_swap_database;
+    string eosio_username;
+    mapping(string => uint256) registered_for_swap_database; // String = eosio_username , uint256 = XBL balance
+    mapping(address=> string) address_to_eosio_username; //String = eosio_username, 
 
-	address public swap_address;
-	address public XBLContract_addr;
-	XBL_ERC20Wrapper private ERC20_CALLS;
+    address public swap_address;
+    address public XBLContract_addr;
+    XBL_ERC20Wrapper private ERC20_CALLS;
 
 
-	constructor() public
-	{
-		swap_address = address(this); /* Own address */
-		XBLContract_addr = 0x49AeC0752E68D0282Db544C677f6BA407BA17ED7;
-		ERC20_CALLS = XBL_ERC20Wrapper(XBLContract_addr);
-	}
-	
+    constructor() public
+    {
+        swap_address = address(this); /* Own address */
+        XBLContract_addr = 0xef55BfAc4228981E850936AAf042951F7b146e41;
+        ERC20_CALLS = XBL_ERC20Wrapper(XBLContract_addr);
+    }
+    
 
-	function registerSwap(uint256 xbl_amount, string memory eosio_username) public returns (int256 STATUS_CODE)
-	{
-		// -1 = allowance mismatch
-		// -2 = balance mismatch
-		if (ERC20_CALLS.allowance(msg.sender, swap_address) < xbl_amount)
+    function registerSwap(uint256 xbl_amount, string memory eosio_username) public returns (int256 STATUS_CODE)
+    {
+        // -1 = allowance mismatch
+        // -2 = balance mismatch
+        if (ERC20_CALLS.allowance(msg.sender, swap_address) < xbl_amount)
             return -1;
 
         if (ERC20_CALLS.balanceOf(msg.sender) < xbl_amount) 
@@ -52,5 +57,28 @@ contract SwapContrak
 
         ERC20_CALLS.transferFrom(msg.sender, swap_address, xbl_amount);
         registered_for_swap_database[eosio_username] = xbl_amount;
-	}
+    }
+    
+    function getPercent(uint8 percent, uint256 number) private returns (uint256 result)
+    {
+        return number * percent / 100;
+    }
+    
+    function getEOSIO_USERNAME(address _owner) public view returns (string memory eosio_username) 
+     {
+        return address_to_eosio_username[_owner];
+     }
+     
+    function getBalanceByEOSIO_USERNAME(string memory eosio_username) public returns (uint256 balance) 
+     {
+        balance = registered_for_swap_database[eosio_username];
+        if (balance > 5000)
+        {
+            return balance *getPercent(5,balance);
+        }
+        else
+        {
+            return balance;    
+        }
+     }
 }
